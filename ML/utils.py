3d33cd053392c12960328de6e990378fe22fd450
Yusheng Cai <cys9741@seas.upenn.edu>
2022-03-17 11:03:19 -0400

    Updated ML utils to include preparation of PIB data

--- ML/utils.py
+++ ML/utils.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 
-def prep_data_PIB(file:str):
+def prep_data_PIB(file:str, dt:int):
     """
     Function that preps the data for PIB reading from a file, the file will have the following format 
     ' # time x1 x2 x3 ... '
@@ -11,5 +11,38 @@ def prep_data_PIB(file:str):
     Input:
     -----
         file(str)       : The input .dat file as a str --> could be a npy file as well
+        dt(int)         : The time step lag 
     """
+    def parseNPY(file:str):
+        data = np.load(file)
+
+        return data
+
+    def parseDAT(file:str):
+        f = open(file, "r")
+        lines = np.array([[float(f) for f in line.nstrip("\n").split()] for line in f.readlines()])
+
+        return lines
+
+    # register the functions into a dictionary
+    loadDic_   = {
+                "npy" : parseNPY,
+                "dat" : parseDAT
+    }
+
+    # automatically read the type of files
     file_split = file.split(".")
+    filetype   = file_split[1]
+
+    # load the parsing function
+    parseFunc  = loadDic_[filetype]
+    parsedData = parseFunc(file)[:,1:]
+
+    # Parsed data will be [time, x1, x2, x3, ... , ]
+    # We will make an X that has [[x1(t),x2(t),x3(t), ..],...] and y that has [[x1(t+dt), x2(t+dt), x3(t+dt), ...],...]
+    X = parsedData[:,:-dt]
+    y = parsedData[:,dt:]
+
+    assert len(X) == len(y) , "The length of X and y do not agree, something is wrong with the code."
+
+    return X, y
