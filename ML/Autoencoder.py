a6695ee7dbd1bf4d814719d09f5acf03d709c110
Yusheng Cai <cys9741@seas.upenn.edu>
2022-03-17 23:26:52 -0400

    Updated autoencoder such that the network for encoder/decoder does not have to be symmetric

--- ML/Autoencoder.py
+++ ML/Autoencoder.py
@@ -1,16 +1,22 @@
 import numpy as np
 import torch.nn as nn
+import torch
 
 class Autoencoder(nn.Module):
     """
-    Dimensions(list)    :   list of dimension for encoder inputted as (input_dim, dim1, dim2, ..., RC_dim), 
-                            decoder dimension will be assumed to be symmetric
+    Encoderdimensions(list) : Encoder dimensions inputted as (Input_d, dim1, dim2 ,....)
+    hidden_dim(int)         : The dimension of the RC 
+    Decoderdimensions(list) : Decoder dimensions inputted as (dim1, dim2, .., Output_d)
     """
-    def __init__(self, dimensions):
+    def __init__(self, Encoderdimensions:list, hidden_dim:int, Decoderdimensions:list):
         super().__init__()
-        self.dimensions = dimensions
-        self.N          = len(self.dimensions)
-        assert self.N > 1 , "Number of layers must be larger than 1 as it consist of (intput_dim, dim1, .. , RC_dim)"
+        self.Encoderdimensions = Encoderdimensions
+        self.Decoderdimensions = Decoderdimensions
+        self.hidden_dim        = hidden_dim
+        self.Nencoder          = len(self.Encoderdimensions)
+        self.Ndecoder          = len(self.Decoderdimensions)
+        assert self.Nencoder >=1  , "Number of layers must be larger or equal to 1 as it consist of (intput_dim, dim1,..)"
+        assert self.Ndecoder >=1  , "Number of layers must be larger or equal to 1 as it consist of (dim1, dim2, ... ouput_dim"
 
         self.encoder = self._encoder_init()
         self.decoder = self._decoder_init()
@@ -20,9 +26,10 @@ class Autoencoder(nn.Module):
         Initialization of the encoder 
         """
         layers = []
-        for i in range(self.N - 1):
-            layers.append(nn.Linear(self.dimensions[i], self.dimensions[i+1]))
+        for i in range(self.Nencoder-1):
+            layers.append(nn.Linear(self.Encoderdimensions[i], self.Encoderdimensions[i+1]))
             layers.append(nn.ReLU())
+        layers.append(nn.Linear(self.Encoderdimensions[-1], self.hidden_dim))
         
         return nn.Sequential(*layers)
 
@@ -30,17 +37,16 @@ class Autoencoder(nn.Module):
         """
         Initialization of the decoder
         """
-        layers = []
+        tempDim = [self.hidden_dim] + list(self.Decoderdimensions)
+        # We know N>=2
+        N = len(tempDim)
 
-        # If the number of layers is larger than 2 --> e.g. 3 layers [10,5,1], then the layer going from 5-->1 does not require a Relu
-        # have to be careful with that
-        if self.N > 2:
-            for i in range(self.N-1, 1,-1):
-                layers.append(nn.Linear(self.dimensions[i], self.dimensions[i-1]))
-                layers.append(nn.ReLU())
-            layers.append(nn.Linear(self.dimensions[1], self.dimensions[0]))
-        else:
-            layers.append(nn.Linear(self.dimensions[1] , self.dimensions[0]))
+        layers = []
+        for i in range(N-2):
+            layers.append(nn.Linear(tempDim[i], tempDim[i+1]))
+            layers.append(nn.ReLU())
+
+        layers.append(nn.Linear(tempDim[-2], tempDim[-1]))
         
         return nn.Sequential(*layers)
 
@@ -58,3 +64,27 @@ class Autoencoder(nn.Module):
         decoded = self.decoder(encoded)
 
         return decoded
+
+    def evaluate(self, X):
+        """
+        Evaluate returns the decoded output
+
+        Input:
+        ------
+            X(torch.tensor)     : A(N,d) array that contains the d-dimensional input data
+
+        Output:
+        -------
+            decoded(torch.tensor): A (N,d) array that contains the d-dimensional output data 
+        """
+        with torch.no_grad():
+            encoded = self.encoder(X)
+            decoded = self.decoder(encoded)
+
+            return decoded
+    
+    def evaluate_hidden_dim(self, X):
+        with torch.no_grad():
+            encoded = self.encoder(X)
+
+            return encoded
