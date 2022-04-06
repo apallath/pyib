5da50fbd8e127f0fd851f02a699579abc4c8f1ea
Yusheng Cai <cys9741@seas.upenn.edu>
2022-04-06 17:18:47 -0400

    Updated code for distributions

--- pyib/ml/Distributions.py
+++ pyib/ml/Distributions.py
@@ -2,10 +2,15 @@ import torch
 import numpy as np
 import torch.nn as nn
 
-def log_Normal_diag(x, log_var, mean, dim=None):
+def log_Normal_diag(x, mean, log_var, dim=None):
     """
     Function that calculates the log-likelihood for a diagonal multivariate gaussian distribution
     """
     log_normal = -0.5 * (log_var + torch.pow(x - mean, 2)/ torch.exp(log_var))
 
-    return torch.sum(log_normal, dim=dim)
+    if dim is not None:
+        sum_ = torch.sum(log_normal, dim=dim)
+    else:
+        sum_ = log_normal
+
+    return sum_
