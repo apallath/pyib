Made major changes to how kl divergence is calculated which was initially a bug

pyib/ml/models.py
@@ -1,7 +1,6 @@
 import numpy as np
 import torch.nn as nn
 import torch
-import logging
 
 from .layers import NonLinear
 from .Distributions import log_Normal_diag
@@ -141,7 +140,6 @@ class VAE(nn.Module):
         self.decoder = self._decoder_init()
         self._representativeInputs_init()
 
-
     def _encoder_init(self):
         """
         Initialization of the encoder 
@@ -194,6 +192,20 @@ class VAE(nn.Module):
 
         # Softmax is used here to make sure that \sum_{i} w_{i} = 1
         self.__weight_layer  = nn.Sequential(nn.Linear(self.output_dim, 1, bias=False), nn.Softmax(dim=0))
+    
+    def RepresentativeInputs_init(self, X:torch.tensor, labels:torch.tensor):
+        """
+        Function used by user to overwrite the bad initial guess for representative input
+
+        X(torch.tensor) : Shape (N, input_dim)
+        labels  : Shape (N,1)
+        """
+        assert X.shape[1] == self.input_dim, "dimension of X is incorrect, it needs to be {}".format(self.input_dim)
+        self.__pseudo_input = torch.zeros(self.representative_dim, self.input_dim, requires_grad=False, device=self.device)
+
+        for i in range(self.representative_dim):
+            index = (labels==i)
+            self.__pseudo_input[i] = X[index,:].mean(axis=0) 
 
     def reparametrize(self, mean, logvar):
         """
@@ -204,7 +216,7 @@ class VAE(nn.Module):
         std = torch.exp(0.5 * logvar)
         eps = torch.rand_like(std, device=self.device)
 
-        return mean + std * eps, eps
+        return mean + std * eps
     
     def _encode(self, X):
         """
@@ -237,10 +249,10 @@ class VAE(nn.Module):
         """
         mean, logvar = self._encode(X)
 
-        decoder_input, z_sample = self.reparametrize(mean, logvar)
-        decoder_output = self.decoder(decoder_input)
+        z_sampled = self.reparametrize(mean, logvar)
+        decoder_output = self.decoder(z_sampled)
 
-        return decoder_output, mean, logvar, z_sample
+        return decoder_output, mean, logvar, z_sampled
     
     def evaluate(self, X, to_numpy=True):
         """
@@ -299,21 +311,21 @@ class VAE(nn.Module):
         representative_Z_mean, representative_Z_logvar = self._encode(uk)
 
         # Shape (1, Representative_D, hidden_dim)
-        representative_Z_mean = representative_Z_mean.unsqueeze(0)
-        representative_Z_logvar = representative_Z_logvar.unsqueeze(0)
+        representative_mean = representative_Z_mean.unsqueeze(0)
+        representative_logvar = representative_Z_logvar.unsqueeze(0)
 
         # expand z  (N,1, hidden_dim)
         z_expand = z.unsqueeze(1)
 
         # Find the log likelihood --> (N, Representative_D)
-        log_p  = log_Normal_diag(z_expand, representative_Z_mean, representative_Z_logvar, dim=2)
+        log_p  = log_Normal_diag(z_expand, representative_mean, representative_logvar, dim=2)
 
         # Obtain the weights w --> which we will then dot with log_p
         # shape (Representative_D, 1)
         w = self.__weight_layer(self.__weights_input)
 
         # Shape (N, 1)
-        log_r  = torch.log(torch.exp(log_p) @ w + 1e-10)
+        log_r  = torch.log(torch.exp(log_p)@w + 1e-10)
 
         return log_r
     
@@ -382,7 +394,7 @@ class VAE(nn.Module):
 
                     # find the center z 
                     # shape (1, hidden_dim)
-                    center_z = z[index].mean().reshape(1,-1)
+                    center_z = z[index].mean(axis=0).reshape(1,-1)
 
                     # find the point closest to point z
                     # shape (N,)
@@ -407,3 +419,6 @@ class VAE(nn.Module):
 
         # reset the pseudo input u_k
         self.__pseudo_input = representative_inputs
+    
+    def get_psuedo_input(self):
+        return self.__pseudo_input

pyib/ml/train_utils.py
@@ -53,6 +53,9 @@ def SPIB_data_prep(file_name:str, dt:int, label_file:str, one_hot=False, train_p
     # read the trajectory
     time, traj = traj_reader.read_traj(skip=skip)
     traj = torch.tensor(traj, dtype=torch.float32)
+    traj = (traj - traj.mean(axis=0))/traj.std(axis=0)
+    traj = traj[:,:2]
+
     N = len(traj) - dt
     num_train = int(N * train_percent)
 
@@ -75,7 +78,7 @@ def SPIB_data_prep(file_name:str, dt:int, label_file:str, one_hot=False, train_p
     test_traj  = traj[testIdx, :2]
     test_labels = label[testIdx + dt]
 
-    return train_traj, train_labels, test_traj, test_labels
+    return train_traj, train_labels, test_traj, test_labels, traj, label
 
 
 def SPIB_train(VAE_model:VAE, filename:str, label_file:str, dt:int, lr=1e-3, update_labels=True, batch_size=1028, epochs=1000, \
@@ -104,7 +107,8 @@ def SPIB_train(VAE_model:VAE, filename:str, label_file:str, dt:int, lr=1e-3, upd
     CrossEntropyLoss = nn.CrossEntropyLoss()
 
     # Prepare input data 
-    TrainX, TrainY, testX, testY = SPIB_data_prep(filename, dt, label_file, skip=skip, comment_str=comment_str, format=format, one_hot=one_hot)
+    TrainX, TrainY, testX, testY, traj, label = SPIB_data_prep(filename, dt, label_file, skip=skip, comment_str=comment_str, format=format, one_hot=one_hot)
+    # data comes normalized
 
     # Move data to device (could be GPU)
     TrainX = TrainX.to(device)
@@ -112,10 +116,6 @@ def SPIB_train(VAE_model:VAE, filename:str, label_file:str, dt:int, lr=1e-3, upd
     testX  = testX.to(device)
     testY  = testY.to(device)
     VAE_model.to(device)
-
-    # Normalize the data 
-    TrainX = (TrainX - TrainX.mean(axis=0))/TrainX.std(axis=0)
-    testX  = (testX - testX.mean(axis=0))/testX.std(axis=0)
 
     # Define dataset, trainloader and testloader
     TrainDataset = Loader(TrainX, TrainY)
@@ -125,6 +125,7 @@ def SPIB_train(VAE_model:VAE, filename:str, label_file:str, dt:int, lr=1e-3, upd
 
     # before training, track the state population --> The first iteration is probably really bad 
     # I usually see it all predicting 1 state [ ...,0,0,0,1,0,0, ...]
+    VAE_model.RepresentativeInputs_init(traj, label)
     _, state_population0 = VAE_model.get_Labels(TrainX)
 
     labels_update_count = 0
@@ -154,7 +155,7 @@ def SPIB_train(VAE_model:VAE, filename:str, label_file:str, dt:int, lr=1e-3, upd
             KLLoss = torch.mean(log_p - log_r, dim=0)
 
             # Accumulate the total loss 
-            totalLoss = CELoss - beta * KLLoss
+            totalLoss = CELoss + beta * KLLoss
             avg_KL_loss += KLLoss.item()
             avg_CE_loss += CELoss.item()
             avg_epoch_loss += totalLoss.item()