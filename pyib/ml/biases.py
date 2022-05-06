950cd24e0fa50ba32feb337f6cacd465733e177c
Akash Pallath <pallathakash@gmail.com>
2022-05-06 14:22:31 -0400

    Added legendre bias tests, changed default tensor type to Double

--- pyib/ml/biases.py
+++ pyib/ml/biases.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
+
+torch.set_default_tensor_type(torch.DoubleTensor)
 
 
 ################################################################################
